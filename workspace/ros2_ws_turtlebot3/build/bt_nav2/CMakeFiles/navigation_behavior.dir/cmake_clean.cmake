6d452d87c92aa801a1750b04d48174e4139239b7
Borella Simone <borellasimone00@gmail.com>
2025-02-28 11:53:49 +0100

    Update .gitignore

---
+++ workspace/ros2_ws_turtlebot3/build/bt_nav2/CMakeFiles/navigation_behavior.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/navigation_behavior.dir/src/navigation_behavior/navigation_behavior.cpp.o"
+  "CMakeFiles/navigation_behavior.dir/src/navigation_behavior/navigation_behavior.cpp.o.d"
+  "navigation_behavior"
+  "navigation_behavior.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/navigation_behavior.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
